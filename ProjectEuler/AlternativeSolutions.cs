446e04d6e4297b35ca64f7623019f2c636e48af3
Emmabyrne36 <emmabyrne36@gmail.com>
2018-07-21 10:54:24 +0100

    Updated solutions

---
+++ ProjectEuler/AlternativeSolutions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class AlternativeSolutions
+    {
+        #region ProjectEuler2
+        public static int Fib(int n)
+        {
+            return n <= 1 ? n : Fib(n - 1) + Fib(n - 2);
+        }
+
+        public static List<int> series = new List<int>();
+        public static void FibSeries(int n)
+        {
+            bool result = true;
+            while (result)
+            {
+                result = FibTemp(1, 2, 1, n);
+            }
+        }
+
+
+        public static bool FibTemp(int a, int b, int counter, int length)
+        {
+            if (counter <= length)
+            {
+                if (a % 2 == 0 && !(a >= 4000000) && !(b >= 4000000))
+                {
+                    series.Add(a);
+                    FibTemp(b, a + b, counter + 1, length);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+
+        #region ProjectEuler3
+        public static long ProjEuler3OtherSolution()
+        {
+            long n = 600851475143;
+            while (true)
+            {
+                long p = SmallestFactor(n);
+                if (p < n)
+                {
+                    n /= p;
+                }
+                else
+                {
+                    return n;
+                }
+            }
+        }
+
+        // Returns the smallest factor of n, which is in the range [2, n]. The result is always prime.
+        public static long SmallestFactor(long n) // from online solution
+        {
+            if (n <= 1)
+            {
+                n = 1;
+            }
+
+            for (long i = 2, end = Convert.ToInt64(Math.Sqrt(n)); i <= end; i++)
+            {
+                if (n % i == 0)
+                {
+                    return i;
+                }
+            }
+            return n;
+        }
+        #endregion
+
+        #region ProjectEuler4
+        public static bool IsPalindrome2(int n)
+        {
+            char[] characterArray = n.ToString().ToCharArray();
+            for (int i = 0; i <= (characterArray.Length / 2); i++)
+            {
+                if (characterArray[i] != characterArray[(characterArray.Length - 1) - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        #endregion
+
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -50,79 +50,11 @@ namespace ProjectEuler
             }
             return sumValues;
         }
-
-        public static int Fib(int n)
-        {
-            return n <= 1 ? n : Fib(n - 1) + Fib(n - 2);
-        }
-
-        public static List<int> series = new List<int>();
-        public static void FibSeries(int n)
-        {
-            bool result = true;
-            while (result)
-            {
-                result = FibTemp(1, 2, 1, n);
-            } 
-        }
-
-        
-        public static bool FibTemp(int a, int b, int counter, int length)
-        {
-            if (counter <= length)
-            {
-                if ( a % 2 == 0 && !(a >= 4000000) && !(b >= 4000000) )
-                {
-                    series.Add(a);
-                    FibTemp(b, a + b, counter + 1, length);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
         #endregion
 
         #region ProjEuler3
         // The prime factors of 13195 are 5, 7, 13 and 29.
         // Get largest prime factor of 600851475143
-        public static long ProjEuler3OtherSolution()
-        {
-            long n = 600851475143;
-            while (true)
-            {
-                long p = SmallestFactor(n);
-                if (p < n)
-                {
-                    n /= p;
-                }
-                else
-                {
-                    return n;
-                }
-            }
-        }
-
-        // Returns the smallest factor of n, which is in the range [2, n]. The result is always prime.
-        public static long SmallestFactor(long n) // from online solution
-        {
-            if (n <= 1)
-            {
-                n = 1;
-            }
-
-            for (long i = 2, end = Convert.ToInt64(Math.Sqrt(n)); i <= end; i++)
-            {
-                if (n % i == 0)
-                {
-                    return i;
-                }
-            }
-            return n;
-        }
-
         public static long ProjEuler3()
         {
             long n = 600851475143;
@@ -214,21 +146,6 @@ namespace ProjectEuler
             }
             return true;
         }
-
-        public static bool IsPalindrome2(int n)
-        {
-            char[] characterArray = n.ToString().ToCharArray();
-            for (int i = 0; i <= (characterArray.Length / 2); i++)
-            {
-                if (characterArray[i] != characterArray[(characterArray.Length - 1) - i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-        
         #endregion
 
         #region ProjeEuler5
